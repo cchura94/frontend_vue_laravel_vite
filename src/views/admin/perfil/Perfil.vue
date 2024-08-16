8c658e5e219c76b4fd0ae766329d1d3c4d188dae
cristian <cchura.cpc@gmail.com>
2024-08-15 22:47:46 -0400

    perfil

--- src/views/admin/perfil/Perfil.vue
+++ src/views/admin/perfil/Perfil.vue
@@ -1,3 +1,34 @@
 <template>
-    <h1>Mi Perfil</h1>
+    <div class="card">
+
+        <h5>NOMBRE: {{ perfil.name }}</h5>
+
+        <h5>CORREO: {{ perfil.email }}</h5>
+
+        <h5>CREADO EN: {{ perfil.created_at }}</h5>
+        
+
+    </div>
 </template>
+
+<script setup>
+    import { onMounted, ref } from "vue";
+    import authService from "./../../../services/auth.service"
+
+    const perfil = ref({});
+
+    onMounted(() => {
+        obtenerPerfil()
+    })
+
+    async function obtenerPerfil(){
+        try {
+            const {data} = await authService.perfil()
+            perfil.value = data;
+            
+        } catch (error) {
+            alert("Error al recuperar los datos de perfil")
+        }
+    }
+
+</script>
